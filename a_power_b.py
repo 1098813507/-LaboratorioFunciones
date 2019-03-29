7c195d0c0d033f3e5beb89af3f7f281f87d4ec3d
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2019-03-29 13:54:00 -0500

    commit

--- a_power_b.py
+++ a_power_b.py
@@ -1,6 +1,10 @@
-def a_power_b (a,b):
-  z= a*b
-  print(z)
-a=int(input("Digite un numero"))
-b=int(input("Digite un numero o muere"))
-a_power_b (a,b)
+def a_power_b(x,y):
+ contador=1
+ for i in range (0,y):
+  contador=contador*x
+ return contador  
+x=int(input("Digite un numero"))
+y=int(input("Digite un numero o muere"))
+
+efe=a_power_b(x,y)
+print(efe)
